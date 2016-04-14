654a2ceca7edf2ab048720a7447d8a602fe91395
Mindi Torrey <minditorrey@me.com>
2016-04-14 08:37:11 -0700

    Fixed Conditional

--- main.js
+++ main.js
@@ -10,7 +10,10 @@ $('.towers').click(clickTower);
 }
 
 function clickTower(e) {
+	console.log ($('.selected').data('size') );
+
 	var $clickedTower = $(this);
+	console.log($(this).children().data('size'));
 	//if this tower does not have selected children and no tower has a selected disk
 	//toggle class selected of first child
 	if($clickedTower.find('.selected').length !==1 && $(".selected").length ===0){
@@ -20,12 +23,12 @@ function clickTower(e) {
 	else if($clickedTower.find('.selected').length !==1 && $(".selected").length !==0){
 			//if selected disk is smaller than clicked tower or clicked tower
 			//has no children disks, remove class and append disk
-			if($('.selected').dataAttribute < $(this).children().dataAttribute || $clickedTower.children().length ===0) {
+			if($('.selected').data('size') < $(this).children().data('size') || $clickedTower.children().length ===0) {
 			var $selectedDisk = $(".selected").remove();
 			$clickedTower.append($selectedDisk);
 			}
 			//if selected disk is larger than children disks then alert: you cannot drop this here
-			} else if($('.selected').dataAttribute > $clickedTower.children().dataAttribute) {
+			} else if($('.selected').data('size') > $clickedTower.children().data('size')) {
 				alert('You cannot drop a disk onto a smaller disk!');			
 			} else {
 		//toggle class selected
